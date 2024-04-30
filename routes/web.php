bb2ff06ce94b8c7405f9f90e5530f751854c25cb
guittex <guilherme.oliveira@asprev.com.br>
2024-04-30 14:09:15 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,4 @@ Route::controller(SorteiosController::class)->group(function () {
     Route::get('/sorteios/view', 'view')->name("sorteios.view");
 });
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
